4dea5134ccfa887e611d3f7dd6ff69fe0218e6cc
Raid <naji.raid@gmail.com>
2021-05-17 14:35:44 -0400

    changed p text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
   <div>
     <h1>Deploying with netlify</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni debitis dignissimos, minus nobis harum doloribus aliquam odio sequi, quo dolores autem dicta quaerat deleniti, reiciendis labore. At magnam voluptas nulla sequi doloribus temporibus consequatur corporis soluta placeat ea, fugiat itaque. Iste eaque, provident temporibus modi maxime accusantium ullam ex quidem saepe minus optio eum fugit impedit blanditiis, quam quaerat necessitatibus. Porro minus tempore adipisci itaque libero quaerat error quam culpa? Eligendi culpa aliquid commodi rem minima, laboriosam aliquam libero aut sequi quam cum! Nobis placeat inventore doloribus. Ipsum dolorum sit illum. Dignissimos, cumque nesciunt quo consectetur doloremque praesentium voluptas eveniet, quas id illo soluta temporibus, doloribus voluptate magni exercitationem sint at assumenda eaque quasi expedita explicabo! Omnis saepe, id exercitationem provident tenetur adipisci nulla, maiores, soluta placeat repellat vitae mollitia eos modi. Quaerat voluptas modi possimus corporis ducimus magnam rerum minus accusantium ex eum, voluptate repellat totam eligendi nemo rem a dicta cupiditate ea sequi accusamus voluptatem aliquam quia dolorem iure. Eos qui modi explicabo laborum magni, corrupti veritatis temporibus eaque! Nemo recusandae optio ea sint. Odio molestias veniam rerum minus voluptatem et nulla perspiciatis iure ad, tenetur tempora quisquam voluptates sint eum tempore quod ut! Dolor porro maiores minus?</p>
+    <p>TESTING NETLIFY DEPLOY</p>
   </div>
   );
 }
